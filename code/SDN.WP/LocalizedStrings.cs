08803ed46d23210a3263d0e25d797b1ea1465e7c
Igor Manushin <igor.manushin@deltacorvi.com>
2015-03-15 17:59:27 +0300

    +WP project

---
+++ code/SDN.WP/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using SDN.WP.Resources;
+
+namespace SDN.WP
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
