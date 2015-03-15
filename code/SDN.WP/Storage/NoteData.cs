cbab7acda1391c97add73f34f433a592cd8b9c15
Igor Manushin <igor.manushin@deltacorvi.com>
2015-03-15 23:52:59 +0300

    +note storage idea

---
+++ code/SDN.WP/Storage/NoteData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDN.WP.Storage
+{
+    internal sealed class NoteData
+    {
+        public Guid Identity
+        {
+            get;
+            private set;
+        }
+
+        public DateTime RemoveAtUtc
+        {
+            get;
+            private set;
+        }
+
+        public ReadOnlyCollection<NoteSnapshot> Snapshots
+        {
+            get;
+            private set;
+        }
+
+        public ReadOnlyDictionary<Guid, byte[]> Images
+        {
+            get;
+            private set;
+        } 
+    }
+}
---
+++ code/SDN.WP/Storage/NoteStorage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDN.WP.Storage
+{
+    internal static class NoteStorage
+    {
+        public static ReadOnlyCollection<NoteData> ActualNotes
+        {
+            get;
+            private set;
+        }
+
+        public static void RemoveNote(Guid noteIdentity)
+        {
+            throw new NotImplementedException(); 
+        }
+
+        public static void AddOrUpdateNote(NoteData note)
+        {
+            throw new NotImplementedException(); 
+        }
+    }
+}
