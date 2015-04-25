944e5f4e0947202f5bb75b1049d9ae23544eb517
Igor Manushin <igor.manushin@deltacorvi.com>
2015-04-26 02:36:10 +0300

    +unit tests

---
+++ code/SDN.WP.UnitTests/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using SDN.WP.UnitTests.Resources;
+
+namespace SDN.WP.UnitTests
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ code/SDN.WP.UnitTests/NoteDataTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using SDN.WP.Storage;
+
+namespace SDN.WP.UnitTests
+{
+    [TestClass]
+    public sealed class NoteDataTest
+    {
+        [TestMethod]
+        public void SerializeDeserializeTest()
+        {
+            var noteData = NoteData.CreateNew();
+
+            var serialized = noteData.Serilize();
+
+            var deserialize = NoteData.Deserialize(serialized);
+
+            var nextSerialized = deserialize.Serilize();
+
+            Assert.AreEqual(serialized, nextSerialized);
+
+            Assert.AreEqual(noteData.Identity, deserialize.Identity);
+            Assert.AreEqual(noteData.RemoveAtUtc, deserialize.RemoveAtUtc);
+            Assert.AreEqual(noteData.Snapshots.Count, deserialize.Snapshots.Count);
+            Assert.AreEqual(noteData.Images.Count, deserialize.Images.Count);
+        }
+    }
+}
