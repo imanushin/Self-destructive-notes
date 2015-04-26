0c3952198f9b52a14e1dbe70eb5fc4df864a6604
Igor Manushin <igor.manushin@deltacorvi.com>
2015-04-26 03:09:15 +0300

    +collections tests

---
+++ code/SDN.Shared.UnitTests/DictionaryExtensionsTest.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Deltacorvi.NetCommon;
+using NUnit.Framework;
+using SDN.Shared.Collections;
+
+namespace Deltacorvi.NetCommon.Tests.Collections
+{
+// ReSharper disable InvokeAsExtensionMethod
+    [TestFixture]
+    public sealed class DictionaryExtensionsTest
+    {
+        private const int key1 = 5;
+        private const int key2 = 12;
+        private const string value1 = "234";
+        private const string value2 = "543";
+
+        [Test]
+        public void AsReadOnly()
+        {
+            var dictionary = new Dictionary<int, string>();
+
+            var result = DictionaryExtensions.ToReadOnlyDictionary(dictionary);
+
+            Assert.IsNotNull(result);
+
+            var theSame = result.ToReadOnlyDictionary();
+
+            Assert.AreSame(result,theSame);
+        }
+
+        [Test]
+        public void TryAdd()
+        {
+            var dictionary = new Dictionary<int, string>();
+
+            bool result = DictionaryExtensions.TryAdd(dictionary, key1, value1);
+            Assert.IsTrue(result);
+
+            result = DictionaryExtensions.TryAdd(dictionary, key1, value1);
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void TryRemove()
+        {
+            var dictionary = new Dictionary<int, string>();
+
+            bool result = DictionaryExtensions.TryRemove(dictionary, key1);
+           Assert.IsFalse(result);
+
+            dictionary.Add(key1, value1);
+
+            result = DictionaryExtensions.TryRemove(dictionary, key1);
+           Assert.IsTrue(result);
+
+            result = DictionaryExtensions.TryRemove(dictionary, key1);
+           Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void TryRemove_Simple()
+        {
+            var dictionary = new Dictionary<int, string>();
+
+            bool result = DictionaryExtensions.TryRemove(dictionary, key1);
+           Assert.IsFalse(result);
+
+            dictionary.Add(key1, value1);
+
+            result = DictionaryExtensions.TryRemove(dictionary, key1);
+           Assert.IsTrue(result);
+
+            result = DictionaryExtensions.TryRemove(dictionary, key1);
+           Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void TryRemove_Out()
+        {
+            var dictionary = new Dictionary<int, string>();
+
+            bool result = DictionaryExtensions.TryRemove(dictionary, key1);
+           Assert.IsFalse(result);
+
+            dictionary.Add(key1, value1);
+
+            string outValue;
+
+            result = DictionaryExtensions.TryRemove(dictionary, key1, out outValue);
+           Assert.IsTrue(result);
+           Assert.AreEqual(value1, outValue);
+
+            result = DictionaryExtensions.TryRemove(dictionary, key1, out outValue);
+           Assert.IsFalse(result);
+           Assert.IsNull(outValue);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void MergeWith_FirstIsNull()
+        {
+            DictionaryExtensions.MergeWith(null, new Dictionary<int, string>(), false);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void MergeWith_SecondIsNull()
+        {
+            DictionaryExtensions.MergeWith(new Dictionary<int, string>(), null, false);
+        }
+
+        [Test]
+        public void MergeWith_OnlyAdd()
+        {
+            var first = new Dictionary<int, string>();
+            var second = new Dictionary<int, string>();
+
+            first.Add(key1, value1);
+            second.Add(key1, value2);
+            second.Add(key2, value2);
+
+            /*int changedCount =*/ DictionaryExtensions.MergeWith(first, second, false);
+
+           //Assert.AreEqual(1, changedCount);
+           Assert.AreEqual(2, first.Count);
+           Assert.AreEqual(value1, first[key1]);
+        }
+
+        [Test]
+        public void MergeWith_AddOrReplace()
+        {
+            var first = new Dictionary<int, string>();
+            var second = new Dictionary<int, string>();
+
+            first.Add(key1, value1);
+            second.Add(key1, value2);
+            second.Add(key2, value2);
+
+            /*int changedCount =*/ DictionaryExtensions.MergeWith(first, second, true);
+
+           //Assert.AreEqual(2, changedCount);
+           Assert.AreEqual(2, first.Count);
+           Assert.AreEqual(value2, first[key1]);
+        }
+
+        [Test]
+        public void GetOrAdd()
+        {
+            var target = new Dictionary<string, int>();
+
+            string key = "key";
+
+            int added = target.GetOrAdd(key);
+
+            Assert.AreEqual(0, added);
+
+            target[key] = 1;
+
+            int getted = target.GetOrAdd(key);
+
+            Assert.AreEqual(1, getted);
+        }
+
+        [Test]
+        public void GetOrNull()
+        {
+            var target = new Dictionary<string, string>();
+
+            string key = "key";
+            string value = "value";
+
+            string firstResult = target.TryGetValue(key);
+
+            Assert.IsNull(firstResult);
+
+            target[key] = value;
+
+            string secondResult = target.TryGetValue(key);
+
+            Assert.AreEqual(value, secondResult);
+        }
+    }
+    // ReSharper restore InvokeAsExtensionMethod
+
+}
---
+++ code/SDN.Shared.UnitTests/EnumerableExtensionsTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using NUnit.Framework;
+using SDN.Shared.Collections;
+
+namespace Deltacorvi.NetCommon.Tests.Collections
+{
+    [TestFixture]
+    public sealed class EnumerableExtensionsTest
+    {
+        [Test]
+        public void ToReadOnlyList()
+        {
+            var input = new[] { 1, 2, 3 };
+
+            var output = input.ToReadOnlyList();
+
+            CollectionAssert.AreEqual(output, input);
+        }
+
+        [Test]
+        public void ForEach_Action()
+        {
+            var input = new[] { "1", null };
+
+            var output = new List<string>();
+
+            input.ForEach(output.Add);
+
+            CollectionAssert.AreEquivalent(input, output);
+        }
+
+        [Test]
+        public void UnionWith()
+        {
+            var original = Enumerable.Range(0, 4);
+            const int newElement = 4;
+            var otherElements = Enumerable.Range(5, 9);
+
+            var result = original.UnionWith(newElement, otherElements.ToArray()).ToReadOnlyList();
+
+            Assert.AreEqual(Enumerable.Range(0, 14).ToReadOnlyList(), result);
+
+        }
+    }
+}
